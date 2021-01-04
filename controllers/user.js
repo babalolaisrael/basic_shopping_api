a01da554c15a067820593b9c326f1532894f7159
IsraelB <babalolaisrael@users.noreply.github.com>
2021-01-04 07:44:00 +0100

    Initial Commit

---
+++ controllers/user.js
@@ -0,0 +1,72 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const dotenv = require('dotenv');
+dotenv.config();
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10).then(
+        (hash) => {
+            const user = new User({
+                email: req.body.email,
+                password: hash
+            });
+            user.save().then(
+              () => {
+                  res.status(201).json({
+                      message: 'User added successfully'
+                  });
+              }  
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        error: error
+                    });
+                }
+            );
+        }
+    );
+};
+
+exports.login = (req, res, next) => {
+    User.findOne({email: req.body.email}).then(
+        (user) => {
+            if (!user) {
+                return res.status(401).json({
+                    error: new Error('User not found!')
+                });
+            }
+            bcrypt.compare(req.body.password, user.password).then(
+                (valid) => {
+                    if (!valid) {
+                        return res.status(401).json({
+                            error: new Error('Incorrect password!')
+                        });
+                    }
+                    const token = jwt.sign(
+                        {userId: user._id},
+                        process.env.RANDOM_TOKEN_SECRET, 
+                        { expiresIn: '24h'}
+                        );
+                    res.status(200).json({
+                        userId: user.id,
+                        token: token
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(500).json({
+                        error: error
+                    });
+                }
+            );
+        }
+    ).catch(
+        (error) => {
+            res.status(500).json({
+                error: error
+            });
+        }
+    );
+};
+
